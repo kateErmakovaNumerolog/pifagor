572c040b6e5fb6f48df0bdfb31babde2f6998d5b
Andrey Vidiborets <45356315+Vidiborets@users.noreply.github.com>
2024-05-05 09:15:25 +0300

    First Deploy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,3 @@
-import { AppBar } from './AppBar';
-import { VideoInfo } from './VideoInfo';
-import { VideoPlayer } from './VideoPlayer';
-
 export const App = () => {
-  return (
-    <div>
-      <AppBar />
-      <VideoPlayer url="https://youtu.be/Pj-h6MEgE7I" />
-      <VideoInfo
-        title="You Are Not Where You Think You Are"
-        views="5,709,844"
-        uploadDate="May 17, 2022"
-      />
-    </div>
-  );
+  return <div>'hello'</div>;
 };
