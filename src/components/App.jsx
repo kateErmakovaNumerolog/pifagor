a0333aa40f3881ca8963ebb6d2e07bf177ab538d
Andrey Vidiborets <45356315+Vidiborets@users.noreply.github.com>
2024-05-25 16:10:50 +0300

    Добавил паддинг

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,7 @@ export const App = () => {
     setValue(birthDate);
   };
   return (
-    <div className=" min-[320px]:p-5 sm:p-5 md:p-5 lg:p-12 xl:p-2">
+    <div className=" min-[320px]:p-2 sm:p-5 md:p-5 lg:p-12 xl:p-2">
       <PickerContainer onSubmitBithDate={onSubmit} />
       <Tabel value={value} />
     </div>
